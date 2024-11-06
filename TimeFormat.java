adb78d1f31cbe1df7ec9be96abe611a660f00512
Ofek30 <ofek.marciano@post.runi.ac.il>
2024-11-06 18:51:22 +0200

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -13,7 +13,7 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         // Replace this comment with the rest of your code
-		if(hours<12)
+		if(hours<13)
 		{
 			if(minutes<10)
 				{
